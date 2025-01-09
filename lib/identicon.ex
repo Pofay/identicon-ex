7d9d15d0f26e6e2680ab45379c03d15ca5e75130
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-01-09 20:30:34 +0800

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,35 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.take(15)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+end
